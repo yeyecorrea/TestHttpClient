2e3a7cc906510722a30e907d7bac711c1d334978
Yeferson Correa <yeyecorrea838@gmail.com>
2021-12-29 15:31:57 -0500

    clase que contiene las costantes urls

---
+++ CostantsUrl.cs
@@ -0,0 +1,9 @@
+namespace TestHttpClient
+{
+    public class CostantsUrl
+    {
+        public static string urlLogin = "https://banking.sandbox.prometeoapi.com/login/";
+        public static string urlLogout = "https://banking.sandbox.prometeoapi.com/logout/";
+        public static string urlClient = "https://banking.sandbox.prometeoapi.com/client/";
+    }
+}
